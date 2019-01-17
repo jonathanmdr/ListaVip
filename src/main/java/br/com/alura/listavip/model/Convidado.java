6926e7986cf36dba484a5860f2935f242b3f50e0
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-01-16 23:09:33 -0200

    Created form for cadaster convided in the listaconvidados view

--- src/main/java/br/com/alura/listavip/model/Convidado.java
+++ src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -13,12 +14,18 @@ import javax.persistence.Id;
 public class Convidado {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
     private String email;
-
     private String telefone;
+
+    public Convidado() {
+    }
+
+    public Convidado(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
 }
